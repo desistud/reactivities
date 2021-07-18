830be7a08cc5d488f118fb283a142da27cef0da2
Vikram Bhavsar <vbhavsar@experianinteractive.com>
2021-07-18 17:55:28 -0500

    End of section 9

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,6 +4,7 @@ import { Grid } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import ActivityList from './ActivityList';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
+import ActivityFilters from './ActivityFilters';
 
 export default observer(function ActivityDashboard() {
   const { activityStore } = useStore();
@@ -23,7 +24,7 @@ export default observer(function ActivityDashboard() {
         <ActivityList />
       </Grid.Column>
       <Grid.Column width="6">
-        <h2>Acitivy Filter</h2>
+        <ActivityFilters/>
       </Grid.Column>
     </Grid>
   );
